f55ec28ee27d5504d983fb15acd6c23bdf4a6b08
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2024-11-25 21:19:46 +0100

    add files

---
+++ src/MatchResult.ts
@@ -0,0 +1,8 @@
+
+
+//enum- enumeration - obiekt który przechowuje zbiur powiązanych ze sobą wartości
+export enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+  };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import fs from 'fs';
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from './MatchResult';
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-})
-.split('\n')
-.map((row: string): string[] => {
-  return row.split(',')
-})
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+const dateOfFirstMatch = reader.data[0][0];
+
+let manUnitedWins = 0;
+
+for (let match of reader.data){
+  if(match[1] === 'Man United' && match[5] ==='H'){
+    manUnitedWins++;
+  }else if(match[2] === 'Man United' && match[5] ==='H'){
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
