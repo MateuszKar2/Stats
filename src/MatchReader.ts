b0dbbe045f5328dd140f029feeeaadc8242bfc29
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2024-11-30 14:10:51 +0100

    add files to my project

---
+++ src/MatchReader.ts
@@ -0,0 +1,19 @@
+import { CsvFileReader } from "./CsvFileReader";
+import { dateStringToDate} from "./utils";
+import { MatchResult } from "./MatchResult";
+
+type MatchData = [Date, string, string, number, number, string];//uporządkowana tablica typów które powinny byc zwracane w tej kolejności
+
+export class MatchReader extends CsvFileReader {
+    .map(row: string[]): MatchData {
+        return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,//asorcja typów
+            row[6]
+        ];
+      }
+}
