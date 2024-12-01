cdcb5a2bf086b5856a45f6443bb5ebae100f7f79
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2024-12-01 11:34:57 +0100

    add change

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -11,16 +11,20 @@ interface DataReader {
 export class MatchReader {
     constructor(public reader: DataReader) {}
 
-    // .map((row: string): MatchData => {
-    //     return [
-    //         dateStringToDate(row[0]),
-    //         row[1],
-    //         row[2],
-    //         parseInt(row[3]),
-    //         parseInt(row[4]),
-    //         row[5] as MatchResult,//asorcja typów
-    //         row[6]
-    //     ];
-    //   }
-    //   )
+    load(): void {
+        this.reader.read();
+        this.reader.data.map(
+            (row: string[]): MatchData => {
+        return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,//asorcja typów
+            row[6]
+        ];
+      }
+      )
+    }
 }
