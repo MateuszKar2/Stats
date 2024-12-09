817864682e3c4732c6e0d540c5caebf087835f9e
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2024-12-09 20:44:41 +0100

    add file

--- src/MatchData.ts
+++ src/MatchData.ts
@@ -1,4 +1,12 @@
 import { MatchResult } from "./MatchResult";
 
 
-export type MatchData = [Date, string, string, number, number, MatchResult, string];//uporządkowana tablica typów które powinny byc zwracane w tej kolejności
+export type MatchData = [
+    Date, 
+    string, 
+    string, 
+    number, 
+    number, 
+    MatchResult, 
+    string
+];//uporządkowana tablica typów które powinny byc zwracane w tej kolejności
