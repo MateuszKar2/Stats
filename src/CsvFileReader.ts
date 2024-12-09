38d4ddfc1f8c1f6c6aa5c523ebafde3da2b68b06
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2024-12-09 07:27:28 +0100

    add changes

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,9 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
-type MatchData = [Date, string, string, number, number, string];//uporządkowana tablica typów które powinny byc zwracane w tej kolejności
 
 export class CsvFileReader {
-    data: MatchData[] = [];
+    data: string[][] = [];
 
     constructor(public filename: string) {}
 
@@ -17,18 +15,6 @@ export class CsvFileReader {
           .split('\n')
           .map((row: string): string[] => {
             return row.split(',')
-          }
-          )
-          .map((row: string): MatchData => {
-            return [
-                dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5] as MatchResult,//asorcja typów
-                row[6]
-            ];
           }
           )
     }
