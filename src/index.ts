30959cfc40d3a16d4ea6f8a0abe58842bd441462
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2024-12-09 20:44:37 +0100

    add file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-const csvFileReader = new CsvFileReader('football.csv');
 
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
 matchReader.load();
-
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new HtmlReport()
-);
-
 summary.buildAndPrintReport(matchReader.matches)
