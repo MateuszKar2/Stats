2b58b02b95b4e4c8431b7473390f7028f19cd715
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2024-12-09 09:18:55 +0100

    add file to my project

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,7 +1,6 @@
+import { MatchResult } from './MatchResult';
 import { dateStringToDate } from './utils';
-
-
-type MatchData = [Date, string, string, number, number, string];//uporządkowana tablica typów które powinny byc zwracane w tej kolejności
+import { MatchData } from './MatchData';
 
 interface DataReader {
     read(): void;
@@ -9,22 +8,24 @@ interface DataReader {
 }
 
 export class MatchReader {
+    matches: MatchData[] = [];
+
     constructor(public reader: DataReader) {}
 
-    load(): void {
-        this.reader.read();
-        this.reader.data.map(
-            (row: string[]): MatchData => {
-        return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,//asorcja typów
-            row[6]
-        ];
-      }
-      )
+    load(): MatchData[] { // Zmieniamy metodę na zwracającą tablicę MatchData
+        this.reader.read(); // Wywołanie metody odczytu danych
+
+        // Używamy map do przetwarzania danych
+        return this.reader.data.map((row: string[]): MatchData => {
+            return [
+                dateStringToDate(row[0]),   
+                row[1],                      
+                row[2],                      
+                parseInt(row[3]),            
+                parseInt(row[4]),            
+                row[5] as MatchResult,      
+                row[6]                       
+            ];
+      });
     }
 }
